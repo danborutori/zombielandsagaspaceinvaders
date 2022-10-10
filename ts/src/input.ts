655ad98c6501c5488c52b5c0fb68e01b695a1ce5
anmc <lamylanmc@hotmail.com>
2022-10-10 16:10:10 +0800

    add onscreen buttons

--- ts/src/input.ts
+++ ts/src/input.ts
@@ -70,6 +70,108 @@ namespace zlsSpaceInvader {
             })
         }
 
+        init(
+            leftButton: HTMLInputElement,
+            rightButton: HTMLInputElement,
+            fireButton: HTMLInputElement
+        ){
+            const updateButton = ()=>{
+                this.left = (leftTouch || leftPointer)
+                this.right = (rightTouch || rightPointer)
+                this.fire = (fireTouch || firePointer)
+            }
+
+            let leftTouch = false
+            let leftPointer = false
+            let rightTouch = false
+            let rightPointer = false
+            let fireTouch = false
+            let firePointer = false
+            leftButton.addEventListener("touchstart", e=>{
+                leftTouch = true
+                this.pressAnyKey = true
+                updateButton()
+            })
+            leftButton.addEventListener("pointerdown", e=>{
+                leftPointer = true
+                this.pressAnyKey = true
+                updateButton()
+            })
+
+            rightButton.addEventListener("touchstart", e=>{
+                rightTouch = true
+                this.pressAnyKey = true
+                updateButton()
+            })
+            rightButton.addEventListener("pointerdown", e=>{
+                rightPointer = true
+                this.pressAnyKey = true
+                updateButton()
+            })
+
+            fireButton.addEventListener("touchstart", e=>{
+                fireTouch = true                
+                this.pressAnyKey = true
+                updateButton()
+            })
+            fireButton.addEventListener("pointerdown", e=>{
+                firePointer = true
+                this.pressAnyKey = true
+                updateButton()
+            })
+
+            leftButton.addEventListener("touchend", e=>{
+                leftTouch = false
+                updateButton()
+            })
+            leftButton.addEventListener("touchcancel", e=>{
+                leftTouch = false
+                updateButton()
+            })
+            leftButton.addEventListener("pointerup", e=>{
+                leftPointer = false
+                updateButton()
+            })
+            leftButton.addEventListener("pointerout", e=>{
+                leftPointer = false
+                updateButton()
+            })
+
+            rightButton.addEventListener("touchend", e=>{
+                rightTouch = false
+                updateButton()
+            })
+            rightButton.addEventListener("touchcancel", e=>{
+                rightTouch = false
+                updateButton()
+            })
+            rightButton.addEventListener("pointerup", e=>{
+                rightPointer = false
+                updateButton()
+            })
+            rightButton.addEventListener("pointerout", e=>{
+                rightPointer = false
+                updateButton()
+            })
+
+            fireButton.addEventListener("touchend", e=>{
+                fireTouch = false
+                updateButton()
+            })
+            fireButton.addEventListener("touchcancel", e=>{
+                fireTouch = false
+                updateButton()
+            })
+            fireButton.addEventListener("pointerup", e=>{
+                firePointer = false
+                updateButton()
+            })
+            fireButton.addEventListener("pointerout", e=>{
+                firePointer = false
+                updateButton()
+            })
+        }
+
         update(){
             this.pressAnyKey = false
             this.maimai = false
