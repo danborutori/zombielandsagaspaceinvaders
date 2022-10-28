b15abf34eab989d797199f625a5456c1e6458599
anmc <lamylanmc@hotmail.com>
2022-10-28 14:04:32 +0800

    enable https

--- tsServer/src/main.ts
+++ tsServer/src/main.ts
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const http = require('http')
+const https = require('https')
 const mime = require('mime-types')
 const LocalStorage = require("node-localstorage").LocalStorage
 const _localStorage: Storage = new LocalStorage('./scratch')
@@ -23,50 +23,56 @@ function readJSON( request: any ){
     })
 }
 
-http.createServer(function (request: any, response: any) {
-    let url: string = request.url
-    if( url.startsWith(appRootPath) || 
-        url == "/favicon.ico"
-    ){
-        if( url.startsWith(appRootPath) )
-            url = url.substring(appRootPath.length)
+https.createServer(
+    {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.crt")
+    },
+    function (request: any, response: any) {
+        let url: string = request.url
+        if( url.startsWith(appRootPath) || 
+            url == "/favicon.ico"
+        ){
+            if( url.startsWith(appRootPath) )
+                url = url.substring(appRootPath.length)
 
-        switch(url){
-        case "":
-        case "/":
-            response.end(`
-            <head>
-                <meta http-equiv="Refresh" content="0; URL=${appRootPath}/index.html">
-            </head>
-            `)
-            break
-        case "/leaderboard":
-            Leaderboard.shared.handle( request, response )
-            break
-        default:
-            // The filename is simple the local directory and tacks on the requested url
-            const filename = rootDirectory+decodeURI(url)
+            switch(url){
+            case "":
+            case "/":
+                response.end(`
+                <head>
+                    <meta http-equiv="Refresh" content="0; URL=${appRootPath}/index.html">
+                </head>
+                `)
+                break
+            case "/leaderboard":
+                Leaderboard.shared.handle( request, response )
+                break
+            default:
+                // The filename is simple the local directory and tacks on the requested url
+                const filename = rootDirectory+decodeURI(url)
 
-            // This line opens the file as a readable stream
-            const readStream = fs.createReadStream(filename)
+                // This line opens the file as a readable stream
+                const readStream = fs.createReadStream(filename)
 
-            // This will wait until we know the readable stream is actually valid before piping
-            readStream.on('open', function () {
-                response.setHeader("Content-Type", mime.lookup(url))
-                // This just pipes the read stream to the response object (which goes to the client)
-                readStream.pipe(response)
-            })
+                // This will wait until we know the readable stream is actually valid before piping
+                readStream.on('open', function () {
+                    response.setHeader("Content-Type", mime.lookup(url))
+                    // This just pipes the read stream to the response object (which goes to the client)
+                    readStream.pipe(response)
+                })
 
-            // This catches any errors that happen while creating the readable stream (usually invalid names)
-            readStream.on('error', function(err: Error) {
-                response.end( err.stack )
-            })
-            break
+                // This catches any errors that happen while creating the readable stream (usually invalid names)
+                readStream.on('error', function(err: Error) {
+                    response.end( err.stack )
+                })
+                break
+            }
+        }else{
+            response.end("Unknown app.")
         }
-    }else{
-        response.end("Unknown app.")
     }
-}).listen(portNumber)   
+).listen(portNumber)   
 
 interface LeaderboardRecord {
     name: string
