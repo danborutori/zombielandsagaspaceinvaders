c1857e1799e6d10139b552f8fd7c481cba512843
anmc <lamylanmc@hotmail.com>
2022-11-18 21:38:57 +0800

    add kaijin boss at wave 30

--- ts/src/wave.ts
+++ ts/src/wave.ts
@@ -1,5 +1,18 @@
 namespace zlsSpaceInvader {
 
+    class EmptyWave extends BaseEnemyWave {
+        readonly isBoss = false
+
+        constructor( readonly onWaveEnd: ()=>void ){
+            super()
+        }
+
+        init(scoreAndCredit: ScoreAndCredit, gameObjectManager: GameObjectManager, playerFlight: PlayerFlight): void {
+            playerFlight.wait(1).then(this.onWaveEnd)
+        }
+
+    }
+
     export class WaveManager {
 
         private enemyWave?: IEnemyWave
@@ -23,8 +36,14 @@ namespace zlsSpaceInvader {
             this.enemyWave && this.enemyWave.clear()
 
             switch( wave ){
+            // case 0:
+            //     this.enemyWave = new EmptyWave( onWaveEnd )
+            //     break
             case 14:
                 this.enemyWave = new Zombie3Wave( onWaveEnd )
+                break
+            case 29:
+                this.enemyWave = new KaijinWave( onWaveEnd )
                 break
             default:
                 this.enemyWave = new EnemyWave(
