0fe9d969ea5906b9af745e6d03aaecde22417a6b
anmc <lamylanmc@hotmail.com>
2022-12-26 13:33:16 +0800

    update onscreen button fix detected image changing over https

--- ts/src/imageButton.ts
+++ ts/src/imageButton.ts
@@ -1,23 +1,42 @@
 namespace zlsSpaceInvader {
 
+    function getUrl( src: string ){
+        return new Promise<string>(resolve=>{
+            const img = new Image()
+            img.src = src
+            img.onload = ()=>{
+                const canvas = document.createElement("canvas")
+                canvas.width = img.width
+                canvas.height = img.height
+                const ctx = canvas.getContext("2d")!
+                ctx.drawImage(img,0,0)
+                resolve( canvas.toDataURL("png") )
+            }
+        })
+    }
+
     export class ImageButton{
+        private upImgUrl: Promise<string>
+        private downImgUrl: Promise<string>
 
         private _down = false
         get down(){ return this._down }
         set down( b: boolean ){
             this._down = b
             if( b )
-                this.input.src = this.downSrc
+                this.downImgUrl.then(s=>this.input.src = s)
             else
-                this.input.src = this.upSrc
+                this.upImgUrl.then(s=>this.input.src = s)
         }
 
         constructor(
             readonly input: HTMLInputElement,
-            readonly upSrc: string,
-            readonly downSrc: string
+            upSrc: string,
+            downSrc: string
         ){
-            input.src = upSrc
+            this.upImgUrl = getUrl(upSrc)
+            this.downImgUrl = getUrl(downSrc)
+            this.upImgUrl.then(s=>input.src = s)            
         }
 
     }
